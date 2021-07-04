0590546bda5db556a7dcde1dbe4cace8cf3a6ebc
Tesfamichael <hope.miky1074@gmail.com>
2021-07-04 15:10:10 +0300

    added animation in home page

--- lib/homePge.dart
+++ lib/homePge.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:techmasterevent/homepageheader.dart';
 import 'package:techmasterevent/projects.dart';
-import 'package:techmasterevent/provider/projectProvider.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -12,18 +11,15 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
 
-  List<Widget> _sc = [HomepageHeader(), ProjectsList()];
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: Column(
-          children: [
-            HomepageHeader(),
-            ProjectsList()
-          ],
-        )
-      ),
+    return Scaffold(
+      body: Column(
+        children: [
+          HomepageHeader(),
+          ProjectsList()
+        ],
+      )
     );
   }
 }
--- lib/homepageheader.dart
+++ lib/homepageheader.dart
@@ -3,182 +3,152 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:sizer/sizer.dart';
 import 'package:techmasterevent/screens/allProjects.dart';
 
-class HomepageHeader extends StatelessWidget {
+class HomepageHeader extends StatefulWidget {
+  @override
+  _HomepageHeaderState createState() => _HomepageHeaderState();
+}
+
+class _HomepageHeaderState extends State<HomepageHeader>
+    with SingleTickerProviderStateMixin {
+  Animation<double> _animation;
+  AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = new AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 1000),
+    );
+
+    _animation = new CurveTween(curve: Curves.ease).animate(_controller);
+    _controller.forward();
+    _controller.addListener(() {
+        setState(() {
+                  print(_animation.value);
+                });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-                child: Row(
-                  children: [
-                    Container(
-                      height: 55.0.h,
-                      width: 75.0.w,
-                      decoration: BoxDecoration(
-                          color: Color(0xFFFAD4B2),
-                          borderRadius: BorderRadius.only(
-                              bottomRight: Radius.circular(20.0.w))),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Container(
-                              width: 6.0.w,
-                              height: 6.0.w,
-                              margin: EdgeInsets.only(left: 5.0.w, top: 5.0.w),
-                              child: Image.asset("assets/menu.png")),
-                          Spacer(),
-                          Container(
-                              width: 50.0.w,
-                              margin: EdgeInsets.only(left: 5.0.w, top: 5.0.w),
-                              child: Image.asset("assets/logo.png")),
-                          Container(
-                              width: 60.0.w,
-                              margin: EdgeInsets.only(left: 8.0.w, top: 2.0.w),
-                              child: Text(
-                                "The world awaits your projects! \nBe creative! \nProject beyond the limits!",
-                                style: GoogleFonts.montserrat(
-                                  fontSize: 10.0.sp,
-                                  color: Color(0XFF434242),
-                                  fontWeight: FontWeight.w300
-                                ),
-                              )),
-                          Spacer(),
-                          Spacer(),
-                          Spacer(),
-                        ],
-                      ),
-                    ),
+      child: Row(
+        children: [
+          Transform.translate(
+                  offset: Offset(0, -20 + _animation.value * 20),
+            child: Container(
+              height: 57.0.h,
+              width: 75.0.w,
+              decoration: BoxDecoration(
+                  color: Color(0xFFFAD4B2),
+                  borderRadius:
+                      BorderRadius.only(bottomRight: Radius.circular(20.0.w))),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                 Transform.translate(
+                    offset: Offset(0, -20 + _animation.value * 20),
+                    child: Container(
+                        width: 6.0.w,
+                        height: 6.0.w,
+                        margin: EdgeInsets.only(left: 5.0.w, top: 10.0.w),
+                        child: Image.asset("assets/menu.png")),
+                  ),
+                  Spacer(),
+                  Transform.translate(
+                    offset: Offset(0, -20 + _animation.value * 20),
+                    child: Container(
+                        width: 50.0.w,
+                        margin: EdgeInsets.only(left: 5.0.w, top: 5.0.w),
+                        child: Image.asset("assets/logo.png")),
+                  ),
+                  Transform.translate(
+                    offset: Offset(0, -20 + _animation.value * 20),
+                    child: Container(
+                        width: 60.0.w,
+                        margin: EdgeInsets.only(left: 8.0.w, top: 2.0.w),
+                        child: Text(
+                          "The world awaits your projects! \nBe creative! \nProject beyond the limits!",
+                          style: GoogleFonts.montserrat(
+                              fontSize: 10.0.sp,
+                              color: Color(0XFF434242),
+                              fontWeight: FontWeight.w300),
+                        )),
+                  ),
+                  Spacer(),
+                  Spacer(),
+                  Spacer(),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            height: 50.0.h,
+            padding: EdgeInsets.only(top: 3.0.h),
+            child: Stack(
+              
+              children: [
 
-                    Container(
-                      height: 50.0.h,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          
-                          Container(
-                            width: 25.0.w,
-                            height: 12.0.w,
-                            child: Center(
-                              child: Container(
-                                  width: 12.0.w,
-                                  height: 12.0.w,
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(10),
-                                    color: Colors.white,
-                                    boxShadow: [
-                                      BoxShadow(
-                                        color: Colors.grey.withOpacity(.1),
-                                        blurRadius: 5,
-                                        spreadRadius: 2,
-                                        offset: Offset(0,4)
-                                      ),
-                                    ]
-                                  ),
-                                  child: Padding(
-                                    padding: const EdgeInsets.all(8.0),
-                                    child: Image.asset("assets/bulb.png"),
-                                  ),
-                                ),
-                            ),
-                          ),
-
-
-                          Container(
-                            width: 25.0.w,
-                            height: 12.0.w,
-                            child: Center(
-                              child: InkWell(
-                                onTap: (){
-                                  Navigator.push(context, MaterialPageRoute(builder: (context) => AllProjectsPage()));
-                                },
-                                child: Container(
-                                    width: 12.0.w,
-                                    height: 12.0.w,
-                                    decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(10),
-                                      color: Colors.white,
-                                      boxShadow: [
-                                        BoxShadow(
-                                          color: Colors.grey.withOpacity(.1),
-                                          blurRadius: 5,
-                                          spreadRadius: 2,
-                                          offset: Offset(0,4)
-                                        ),
-                                      ]
-                                    ),
-                                    child: Padding(
-                                      padding: const EdgeInsets.all(8.0),
-                                      child: Image.asset("assets/geer.png"),
-                                    ),
-                                  ),
-                              ),
-                            ),
-                          ),
-
-
-                          Container(
-                            width: 25.0.w,
-                            height: 12.0.w,
-                            child: Center(
-                              child: Container(
-                                  width: 12.0.w,
-                                  height: 12.0.w,
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(10),
-                                    color: Colors.white,
-                                    boxShadow: [
-                                      BoxShadow(
-                                        color: Colors.grey.withOpacity(.1),
-                                        blurRadius: 5,
-                                        spreadRadius: 2,
-                                        offset: Offset(0,4)
-                                      ),
-                                    ]
-                                  ),
-                                  child: Padding(
-                                    padding: const EdgeInsets.all(8.0),
-                                    child: Image.asset("assets/phone.png"),
-                                  ),
-                                ),
-                            ),
-                          ),
-
-                          Container(
-                            width: 25.0.w,
-                            height: 12.0.w,
-                            child: Center(
-                              child: Container(
-                                  width: 12.0.w,
-                                  height: 12.0.w,
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(10),
-                                    color: Colors.white,
-                                    boxShadow: [
-                                      BoxShadow(
-                                        color: Colors.grey.withOpacity(.1),
-                                        blurRadius: 5,
-                                        spreadRadius: 2,
-                                        offset: Offset(0,4)
-                                      ),
-                                    ]
-                                  ),
-                                  child: Padding(
-                                    padding: const EdgeInsets.all(8.0),
-                                    child: Image.asset("assets/signin.png"),
-                                  ),
-                                ),
-                            ),
-                          ),
-
-
-                        ],
-                      ),
-                    ),
-
-
-
-                    
-                  ],
+                Transform.translate(
+                  offset: Offset(0, -20 + _animation.value * 20),
+                  child: SideButton(imagepath: "assets/bulb.png",),
                 ),
-              );
+
+                Transform.translate(
+                  offset: Offset(0, _animation.value * 14.0.h  ),
+                  child: SideButton(imagepath: "assets/geer.png",),
+                ),
+
+                Transform.translate(
+                  offset: Offset(0, _animation.value * 27.0.h),
+                  child: SideButton(imagepath: "assets/phone.png",),
+                ),
+
+                Transform.translate(
+                  offset: Offset(0, _animation.value * 40.0.h),
+                  child: SideButton(imagepath: "assets/signin.png",),
+                ),
+    
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+class SideButton extends StatelessWidget {
+  final String imagepath;
+  const SideButton({this.imagepath});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+            width: 25.0.w,
+            height: 12.0.w,
+            child: Center(
+              child: Container(
+                width: 12.0.w,
+                height: 12.0.w,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.grey.withOpacity(.1),
+                          blurRadius: 5,
+                          spreadRadius: 2,
+                          offset: Offset(0, 4)),
+                    ]),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Image.asset(imagepath),
+                ),
+              ),
+            ),
+          );
   }
 }
