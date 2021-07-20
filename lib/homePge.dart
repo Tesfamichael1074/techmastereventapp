6f9df103dd3d07c4d456b613a7764f0df4e7e4d1
Tesfamichael1074 <hope.miky1074@gmail.com>
2021-07-20 12:29:55 +0300

    someupdates on home page

--- lib/homePge.dart
+++ lib/homePge.dart
@@ -16,40 +16,41 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
-  List<Widget> _sc = [HomepageHeader(), ProjectsList(height: 40.0.h,)];
+  List<Widget> _sc = [
+    HomepageHeader(),
+    ProjectsList(
+      height: 40.0.h,
+    )
+  ];
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: Container(
-          // height: 100.0.h,
-          // width: 100.0.w,
-          child: CustomScrollView(
-            physics: const BouncingScrollPhysics(
-            parent: AlwaysScrollableScrollPhysics()),
-            slivers: [
-              CupertinoSliverRefreshControl(
-                refreshTriggerPullDistance: 80.0,
-                refreshIndicatorExtent: 60.0,
-                onRefresh: () async {
-                  Provider.of<ProjectProvider>(context, listen: false).fetchProjects();
+    return Scaffold(
+      body: Container(
+        // height: 100.0.h,
+        // width: 100.0.w,
+        child: CustomScrollView(
+          physics: const BouncingScrollPhysics(
+              parent: AlwaysScrollableScrollPhysics()),
+          slivers: [
+            CupertinoSliverRefreshControl(
+              refreshTriggerPullDistance: 80.0,
+              refreshIndicatorExtent: 60.0,
+              onRefresh: () async {
+                Provider.of<ProjectProvider>(context, listen: false)
+                    .fetchProjects();
+              },
+            ),
+            SliverList(
+              delegate: SliverChildBuilderDelegate(
+                (BuildContext ctx, int index) {
+                  return _sc[index];
                 },
+                childCount: 2,
               ),
-              SliverList(
-                delegate: SliverChildBuilderDelegate(
-                  (BuildContext ctx, int index) {
-                    return _sc[index];
-                  },
-                  childCount: 2,
-                ),
-              ),
-
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
   }
 }
-
